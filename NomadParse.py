b39c29be769f11bb5bf351f859bec023590e4862
Neema Nejad <dnnejad@gmail.com>
2018-12-22 01:27:42 -0800

    removed thread argument from command line

--- NomadParse.py
+++ NomadParse.py
@@ -53,7 +53,7 @@ def download(containers, footer_list, user_url, user_dir):
     total_files = len(containers)
 
     # Creating individual files under they're own name
-    for footer in enumerate(footer_list[:100], start=0):
+    for footer in enumerate(footer_list, start=0):
         # Grabbing current file number
         file_num = footer[0] + 1
 
