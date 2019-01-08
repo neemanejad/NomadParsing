615f80709fe2294b88609213bd6c0661e53d7f6c
Neema Nejad <dnnejad@gmail.com>
2019-01-08 15:44:14 -0800

    cleaned up code and delete files created in case of no internet

--- NomadParse.py
+++ NomadParse.py
@@ -2,8 +2,9 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup as soup
 import os, sys, time, requests, argparse, threading, urllib, ssl, multiprocessing
 
+
 def check_dir(user_dir):
-    if (os.path.isdir(user_dir) == False):
+    if not os.path.isdir(user_dir):
         try:
             os.mkdir(user_dir)
         except PermissionError:
@@ -18,20 +19,17 @@ def check_dir(user_dir):
         os.chdir(user_dir)
     return user_dir
 
+
 def check_url(user_url):
     sys.stdout.write("[Nomad]:   Checking \"{}\"\n".format(user_url))
     sys.stdout.flush()
-    try:
-        r = requests.get(user_url)
-    except:
-        print("[Nomad]:   URL does not exist or unreachable\n"
-              "[Nomad]:   Usage: ./NomadParse [PATH] [URL]")
-        sys.exit()
-    if (r.status_code != 200):
+    r = requests.get(user_url)
+    if r.status_code != 200:
         print("[Nomad]:   URL does not exist or unreachable\n"
               "[Nomad]:   Usage: ./NomadParse [PATH] [URL]")
         sys.exit()
     return user_url
+
 
 def footer(containers):
     footer_list = []
@@ -39,6 +37,7 @@ def footer(containers):
         footer_url = container.a["href"]
         footer_list.append(footer_url)
     return footer_list
+
 
 def existing(footer_list):
     # Count how many files already exist in directory
@@ -51,13 +50,15 @@ def existing(footer_list):
             total_size += float(file_size) / 1000000
     return exist, total_size
 
+
 def file_progress_count(footer_list):
     # Get ending download count and size
     downloads = 0
     for footer in footer_list:
-        if (os.path.isfile(footer) == True):
+        if os.path.isfile(footer):
             downloads += 1
     return downloads
+
 
 def download(footer_list, user_url, user_dir):
     # Get total files
@@ -66,7 +67,7 @@ def download(footer_list, user_url, user_dir):
     # Creating individual files under they're own name
     for footer in enumerate(footer_list):
         # Checking if file is already in Directory and displaying progress
-        if (os.path.isfile(footer[1]) == True):
+        if os.path.isfile(footer[1]):
             continue
 
         # Bypass SSL error
@@ -80,7 +81,10 @@ def download(footer_list, user_url, user_dir):
             file.write(source)
             file.close()
         except:
-            continue
+            try:
+                os.remove(footer[1])
+            except FileNotFoundError:
+                continue
 
         # Grabbing current file number
         dwnld_count = file_progress_count(footer_list)
@@ -91,15 +95,17 @@ def download(footer_list, user_url, user_dir):
                          (os.path.basename(user_dir), dwnld_count, total_files, progress))
         sys.stdout.flush()
 
+
 def download_stats(footer_list):
     # Get ending download count and size
     downloads = 0
     total_size = 0
     for footer in footer_list:
-        if (os.path.isfile(footer) == True):
-            if (os.path.getsize(footer) == 0):
+        if os.path.isfile(footer):
+            if os.path.getsize(footer) == 0:
                 os.remove(footer)
                 continue
+
             else:
                 downloads += 1
                 file_size = os.path.getsize(footer)
@@ -107,16 +113,18 @@ def download_stats(footer_list):
     no_download = len(footer_list) - downloads
     return downloads, total_size, no_download
 
+
 def end_summary(downloads, total_size, no_downloads, minutes, seconds):
     # Notify the user that downloads have finished
     print("       |Download Statistics for \"%s\"|              " % os.path.basename(os.getcwd()))
     print("_____________________________________________________\n")
     print("[Nomad]:   Files downloaded: %d" % downloads)
     print("[Nomad]:   Total download size: %.4f MB" % total_size)
-    if (no_downloads > 0):
+    if no_downloads > 0:
         print("[Nomad]:   Files failed to download: {}".format(no_downloads))
     print("[Nomad]:   Elapsed time: %d:%.2d" % (minutes, seconds))
     print("_____________________________________________________\n")
+
 
 def main():
     # Command-line input validation
@@ -194,5 +202,6 @@ def main():
 
     # Show download summary
     end_summary(net_downloads, net_size, no_downloads, minutes, seconds)
+
 
 main()
